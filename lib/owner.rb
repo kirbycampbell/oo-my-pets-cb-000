87832eca1e510c7056e18caa2813a544eb551382
Kirby Campbell <jkirbycampbell@gmail.com>
2018-05-09 17:09:27 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,13 +1,15 @@
 class Owner
-attr_accessor :name, :pets
+attr_accessor :name, :pets, :dog, :cat, :fish
+@counter = 0
 
   def initialize(name)
     @name = name
     @pets = {fishes: [], cats: [], dogs: []}
+    @counter += 1
   end
 
   def buy_fish(name)
-
+    new_fish = Fish.new
   end
 
   def buy_cat(name)
