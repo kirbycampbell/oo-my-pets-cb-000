6332e0bdcfd182eab952ca648427c9ea9bf3c589
Kirby Campbell <jkirbycampbell@gmail.com>
2018-05-09 17:49:50 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -18,11 +18,11 @@ attr_reader :species
   end
 
   def buy_cat(name)
-
+    pets[:cats] << Cat.new(name)
   end
 
   def buy_dog(name)
-
+    pets[:dogs] << Dog.new(name)
   end
 
   def self.reset_all
